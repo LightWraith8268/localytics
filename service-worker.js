d23ed8e6ef23f5194630a960e5408ab934de4806
Patrick Antrobus <pantrobus@aims.edu>
2025-09-23 09:36:13 -0600

    ci(pages): simplify to single-job deploy with configure-pages; bump to v1.2.16 (SW wb-1.2.16)

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 /* global workbox */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
-const VERSION = 'wb-1.2.15';
+const VERSION = 'wb-1.2.16';
 const PRECACHE = [
   './',
   './index.html',
